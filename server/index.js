31de351290fa887573ad682e7d68b0fc82c8db95
Damian Evans <damian@bluesky.eu.com>
2020-04-09 22:32:45 +0100

    Completed to end of chapter 115

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const keys = require('./keys');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const redis = require('redis');
 
 const app = express();
 app.use(cors());
@@ -20,3 +21,41 @@ pgClient.on('error', () => console.log('PG Connection error'));
 pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT')
     .catch(err => console.log(err));
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+app.get('/', (req, res) => {
+    res.send('Well hello there!');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (res, req) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    })
+});
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+    if(parseInt(index) > 40) {
+        return res.status(422).send('Index value too high');
+    }
+
+    redisClient.hset('values', index, 'Nothing Yet!');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) values($1)', [index]);
+
+});
+
+app.listen(5000, err => {
+    console.log('Listening');
+})
